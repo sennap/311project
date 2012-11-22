b874695a11be59ade8dafd343c6e2962afe57b3d
Austin Bohn <austinjbohn@gmail.com>
2012-11-22 10:25:57 -0500

    Tables created and foreign keys written

--- pats_constraints.sql
+++ pats_constraints.sql
@@ -3,3 +3,17 @@
 -- by (student_1) & (student_2)
 --
 --
+
+alter table pets add constraint foreign_animal foreign key (animal_id) references animals(id);
+alter table pets add constraint foreign_owner foreign key (owner_id) references owners(id);
+alter table visits add constraint foreign_pet foreign key (pet_id) references pets(id);
+alter table medicine_costs add constraint foreign_medicine foreign key (medicine_id) references medicines(id);
+alter table animal_medicines add constraint foreign_animal foreign key (animal_id) references animals(id);
+alter table animal_medicines add constraint foreign_medicine foreign key (medicine_id) references medicines(id);
+alter table visit_medicines add constraint foreign_visit foreign key (visit_id) references visits(id);
+alter table visit_medicines add constraint foreign_medicine foreign key (medicine_id) references medicines(id);
+alter table treatments add constraint foreign_visit foreign key (visit_id) references visits(id);
+alter table treatments add constraint foreign_procedure foreign key (procedure_id) references procedures(id);
+alter table procedure_costs add constraint foreign_procedure foreign key (procedure_id) references procedures(id);
+
+
--- pats_structure.sql
+++ pats_structure.sql
@@ -3,3 +3,134 @@
 -- by (student_1) & (student_2)
 --
 --
+
+create table owners(
+	id serial primary key,
+	first_name character varying(255),
+	last_name character varying(255),
+	street character varying(255),
+	city character varying(255),
+	state character varying(255),
+	zip integer,
+	phone integer,
+	email character varying(255),
+	active boolean
+);
+-- should phone be integer or varchar? given that input might include dashes
+
+create table pets(
+	id serial primary key,
+	name character varying(255),
+	animal_id integer,
+	owner_id ingteger,
+	female boolean,
+	date_of_birth date,
+	active boolean
+);
+
+create table visits(
+	id serial primary key,
+	pet_id integer,
+	date date,
+	weight integer,
+	overnight_stay boolean,
+	total_charge integer
+);
+
+create table animals(
+	id serial primary key,
+	name character varying(255),
+	active boolean
+);
+
+create table medicines(
+	id serial primary key,
+	name character varying(255),
+	description character varying(255),
+	stock_amount integer,
+	method character varying(255),
+	unit character varying(255),
+	vaccine boolean
+);
+
+create table medicine_costs(
+	id serial primary key,
+	medicine_id integer,
+	cost_per_unit integer,
+	start_date date,
+	end_date date
+);
+
+create table animal_medicines(
+	id serial primary key,
+	animal_id integer,
+	medicine_id integer,
+	recommended_num_of_units integer
+);
+
+create table visit_medicines(
+	id serial primary key,
+	visit_id integer,
+	medicine_id integer,
+	units_given integer,
+	discount integer  -- is this the right type?
+);	
+
+
+
+create table procedures(
+	id serial primary key,
+	name character varying(255),
+	description character varying(255), --should this be bigger?
+	length_of_time integer --right type?
+	active boolean
+);
+
+
+create table treatments(
+	id serial primary key,
+	visit_id integer,
+	procedure_id integer,
+	successful boolean,
+	discount integer
+);
+
+
+
+create table procedure_costs(
+	id serial primary key,
+	procedure_id integer,
+	cost integer,
+	start_date date
+	end_date date
+);
+
+create table notes(
+	id serial primary key,
+	notable_type character varying(255), -- right type?
+	notable_id integer,
+	title character varying(255),
+	content character varying(255),
+	user_id integer,
+	date date
+);
+
+create table users(
+	id serial primary key,
+	first_name character varying(255),
+	last_name character varying(255),
+	role character varying(255),
+	username character varying(255),
+	password_digest character varying(255), -- right type or is this a hash or something?
+	active boolean
+);
+
+
+
+
+
+
+
+
+
+
